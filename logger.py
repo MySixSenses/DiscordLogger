b150406853f15940aac3c6dc9903bdf58822fe99
MySixSenses <66916362+MySixSenses@users.noreply.github.com>
2020-06-14 09:54:07 -0700

    Update logger.py

--- logger.py
+++ logger.py
@@ -3,10 +3,10 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("The bot is ready!")
-    await client.change_presence(activity = discord.Game(name = "*help|Logging whatever you say"))
+    await client.change_presence(activity = discord.Game(name = "Logging whatever you say"))
 @client.event
 async def on_message(message):
-    channel = client.get_channel(721085548868927560)
+    channel = client.get_channel("Replace this with the ID of the Audit channel")
     sender_id = message.author.id
     message_content = message.content
     if message.author == client.user:
@@ -17,5 +17,5 @@ async def on_message(message):
         except AttributeError:
             await message.channel.sent("I was unable to log the comment")
     
-client.run("NzIxMDg0MTEzODM0MDgyMzU2.XuPX7w.7kVNxWxfTjaM-4Xi_tymHmy56wY")
+client.run("Token here, because mine isn't for your eyes")
     
